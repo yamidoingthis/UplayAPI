8274a7d92b5351a0c76f63df650162c8f902c55c
Thakur Jai Abhijit <jaithakur1905@gmail.com>
2024-02-02 03:19:36 +0800

    added migration for user

---
+++ UplayAPI/Migrations/20240201181404_Register.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UplayAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class Register : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "BirthDate",
+                table: "Users",
+                type: "datetime(6)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "NRIC",
+                table: "Users",
+                type: "varchar(4)",
+                maxLength: 4,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Phone",
+                table: "Users",
+                type: "varchar(8)",
+                maxLength: 8,
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "BirthDate",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "NRIC",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Phone",
+                table: "Users");
+        }
+    }
+}
